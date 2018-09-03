36c3002482f7a4445ee4020916dc9a9a5760925b
bas <bas@klavertje4.org>
2018-09-03 21:21:48 +0200

    adds Factory methods for Pints and Vectors

--- lib/tuples.go
+++ lib/tuples.go
@@ -1,11 +1,15 @@
 package lib
 
+import "math"
+
+const tolerance = 1e-9
+
 // Tuple models a point (w = 1.0) or vector (w = 0.0)
 type Tuple struct {
-	X float32
-	Y float32
-	Z float32
-	W float32
+	X float64
+	Y float64
+	Z float64
+	W float64
 }
 
 // IsPoint checks if the Tuple is a Point
@@ -16,4 +20,22 @@ func (t Tuple) IsPoint() bool {
 // IsVector checks if the Tuple is a Vector
 func (t Tuple) IsVector() bool {
 	return !t.IsPoint()
+}
+
+// Equals checks if two Tuples are equal
+func (t Tuple) Equals(other Tuple) bool {
+	return math.Abs(t.X-other.X) < tolerance &&
+		math.Abs(t.Y-other.Y) < tolerance &&
+		math.Abs(t.Z-other.Z) < tolerance &&
+		math.Abs(t.W-other.W) < tolerance
+}
+
+// Point creates a new Point type Tuple
+func Point(x float64, y float64, z float64) Tuple {
+	return Tuple{x, y, z, 1.0}
+}
+
+// Vector creates a new Vector type Tuple
+func Vector(x float64, y float64, z float64) Tuple {
+	return Tuple{x, y, z, 0.0}
 }
--- lib/tuples_test.go
+++ lib/tuples_test.go
@@ -65,3 +65,31 @@ func Test_Tuple_with_w_0_is_vector(t *testing.T) {
 		t.Errorf("a.IsVector == %t, want %t", a.IsVector(), true)
 	}
 }
+
+// Scenario: "point" describes tuples with w=1
+// Given p ← point(4, -4, 3)
+// Then p = tuple(4, -4, 3, 1)
+func Test_Point_describes_tuples_with_w_1(t *testing.T) {
+	// Given
+	p := Point(4, -4, 3)
+	// Expected
+	wanted := Tuple{4, -4, 3, 1.0}
+	// Then
+	if !p.Equals(wanted) {
+		t.Errorf("Point(4, -4, 3) == %v, want %v", p, wanted)
+	}
+}
+
+// Scenario: "vector" describes tuples with w=0
+// Given v ← vector(4, -4, 3)
+// Then v = tuple(4, -4, 3, 0)
+func Test_Vector_describes_tuples_with_w_1(t *testing.T) {
+	// Given
+	p := Vector(4, -4, 3)
+	// Expected
+	wanted := Tuple{4, -4, 3, 0.0}
+	// Then
+	if !p.Equals(wanted) {
+		t.Errorf("Vector(4, -4, 3) == %v, want %v", p, wanted)
+	}
+}
