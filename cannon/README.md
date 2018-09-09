28db6f7e1285705d4fca577ea7f6ee81bfb58aa6
bas <bas@klavertje4.org>
2018-09-09 20:45:19 +0200

    cannon works

---
+++ cannon/README.md
@@ -0,0 +1,10 @@
+# go-raytracer cannon
+
+small cannon projectile example
+
+Consists of:
+
+- a projectile (position (Point) and speed (Vector))
+- a world (gravity (Vector) and wind (Vector))
+
+Every tick, the new position and speed is calculated, and printed on Stdout
--- cannon/cannon.go
+++ cannon/cannon.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/bas-velthuizen/go-raytracer/tuples"
 )
 
 type projectile struct {
-	position Tuple
-	speed    Tuple
+	position tuples.Tuple
+	speed    tuples.Tuple
 }
 
 type world struct {
-	wind    Tuple
-	gravity Tuple
+	wind    tuples.Tuple
+	gravity tuples.Tuple
 }
 
 func main() {
-	p := projectile{Point(0, 0, 10), Vector(1, 0, 0)}
-	w := world{Vector(0, 0, -0.1), Vector(0, 0, 0)}
+	p := projectile{tuples.Point(0, 0, 10), tuples.Vector(1, 0, 0)}
+	w := world{tuples.Vector(0, 0, -0.9), tuples.Vector(-0.5, 0, 0)}
 	tick := 0
 
-	for p.Z > 0 {
+	for p.position.Z > 0 {
 		tick = tick + 1
 		p.position = p.position.Add(p.speed)
 		p.speed = p.speed.Add(w.gravity).Add(w.wind)
