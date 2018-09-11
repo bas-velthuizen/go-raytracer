45606dad48da1184c2e66c250fd8671b7a6efd7b
Bas Velthuizen <velb@alfen.com>
2018-09-11 09:04:25 +0200

    add canvas to PPM conversion basics

--- canvas/canvas.go
+++ canvas/canvas.go
@@ -1,7 +1,9 @@
 package canvas
 
 import (
+	"fmt"
 	"github.com/bas-velthuizen/go-raytracer/colors"
+	"math"
 )
 
 // Canvas implements a canvas on which bitmap images can be projected/drawn
@@ -9,6 +11,10 @@ type Canvas struct {
 	Width  int
 	Height int
 	grid   []colors.Color
+}
+
+type PPM struct {
+	Lines []string
 }
 
 // NewCanvas creates a new Canvas and returns a pointer to it
@@ -20,7 +26,41 @@ func NewCanvas(width int, height int) *Canvas {
 	return &c
 }
 
-// Pixel returns the value of the pixel at the specified position
+// Get returns the value of the pixel at the specified position
 func (c Canvas) Get(x int, y int) colors.Color {
 	return c.grid[y*c.Width+x]
+}
+
+// Set sets the value of the pixel at the specified position
+func (c Canvas) Set(x int, y int, color colors.Color) {
+	c.grid[y*c.Width+x] = color
+}
+
+// ToPPM creates a PPM data structure of the Canvas
+func (c Canvas) ToPPM() PPM {
+	lines := []string{"P3", "5 3", "255"}
+	for row := 0; row < c.Height; row++ {
+		line := ""
+		for col := 0; col < c.Width; col++ {
+			color := c.Get(col, row)
+			toPPMColorComponent(color.Red)
+
+			line += fmt.Sprintf("%d %d %d", toPPMColorComponent(color.Red), toPPMColorComponent(color.Green), toPPMColorComponent(color.Blue))
+			if col != c.Width-1 {
+				line += " "
+			}
+		}
+		lines = append(lines, line)
+	}
+	return PPM{lines}
+}
+
+func toPPMColorComponent(component float64) uint {
+	value := int(math.Round(255 * component))
+	if value < 0 {
+		value = 0
+	} else if value > 255 {
+		value = 255
+	}
+	return uint(value)
 }
--- canvas/canvas_test.go
+++ canvas/canvas_test.go
@@ -37,3 +37,79 @@ func Test_Create_Canvas(t *testing.T) {
 // And red ← color(1, 0, 0)
 // When write_pixel(c, 2, 3, red)
 // Then pixel_at(c, 2, 3) = red
+func Test_Write_to_Canvas(t *testing.T) {
+	// Given
+	c := NewCanvas(10, 20)
+	red := colors.Color{Red: 1, Green: 0, Blue: 0}
+	// When
+	c.Set(2, 3, red)
+	// then
+	pixel := c.Get(2,3)
+	if !red.Equals(pixel) {
+		t.Errorf("c.Pixel(%d,%d) == %v, want %v", 2, 3, pixel, red)
+	}
+}
+
+// Scenario: Constructing the PPM header
+// Given c ← canvas(5, 3)
+// When ppm ← canvas_to_ppm(c)
+// Then lines 1-3 of ppm are
+// """
+// P3
+// 5 3
+// 255
+// """
+func Test_Construct_PPM_Header(t *testing.T) {
+	// Given
+	c:= NewCanvas(5, 3)
+	// When
+	ppm := c.ToPPM()
+	// Expected
+	lines :=   []string{"P3","5 3","255"}
+	// Then
+	for i := 0; i < 3; i++ {
+		if ppm.Lines[i] != lines[i] {
+			t.Errorf("line[%d] == %s, want %s", i, ppm.Lines[i], lines[i])
+		}
+	}
+}
+
+// Scenario: Constructing the PPM pixel data
+// Given c ← canvas(5, 3)
+// And c1 ← color(1.5, 0, 0)
+// And c2 ← color(0, 0.5, 0)
+// And c3 ← color(-0.5, 0, 1)
+// When write_pixel(c, 0, 0, c1)
+// And write_pixel(c, 2, 1, c2)
+// And write_pixel(c, 4, 2, c3)
+// And ppm ← canvas_to_ppm(c)
+// Then lines 4-6 of ppm are
+// """
+// 255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+// 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255
+// """
+func Test_Construct_PPM_Pixel_Data(t *testing.T) {
+	// Given
+	c:= NewCanvas(5, 3)
+	c1 := colors.Color{ Red: 1.5,  Green: 0,   Blue: 0}
+	c2 := colors.Color{ Red: 0,    Green: 0.5, Blue: 0}
+	c3 := colors.Color{ Red: -0.5, Green: 0,   Blue: 1}
+	// When
+	c.Set(0, 0, c1)
+	c.Set(2, 1, c2)
+	c.Set(4, 2, c3)
+	ppm := c.ToPPM()
+	// Expected
+	lines := []string{
+		"255 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+		"0 0 0 0 0 0 0 128 0 0 0 0 0 0 0",
+		"0 0 0 0 0 0 0 0 0 0 0 0 0 0 255",
+	}
+	// Then
+	for i := 3; i < 6; i++ {
+		if ppm.Lines[i] != lines[i - 3] {
+			t.Errorf("line[%d] == %s, want %s", i, ppm.Lines[i], lines[i-3])
+		}
+	}
+}
