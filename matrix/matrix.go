8af031630e207a6f0bf79f674da6e6e5aae9bef6
Bas Velthuizen <b.velthuizen@alfen.com>
2018-09-12 09:07:28 +0200

    add tests and Equal Matrix method

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -6,21 +6,21 @@ import (
 
 // Matrix is a two dimensional array
 type Matrix struct {
-	width  int
-	height int
+	size  int
 	data   []float64
 }
 
-// NewMatrix constructs a new Matrix from the provided data
+// NewMatrix constructs a new square Matrix from the provided data
 func NewMatrix(data [][]float64) *Matrix {
-	if len(data) == 0 {
-		return &Matrix{width: 0, height: 0, data: []float64{}}
+	size := len(data)
+	if size == 0 {
+		return &Matrix{size: 0, data: []float64{}}
 	}
-	result := Matrix{width: len(data[0]), height: len(data), data: make([]float64, len(data)*len(data[0]))}
+	result := Matrix{size: size, data: make([]float64, size * size)}
 
-	for y := 0; y < result.height; y++ {
-		for x := 0; x < result.width; x++ {
-			result.data[y*result.width+x] = data[x][y]
+	for y := 0; y < size; y++ {
+		for x := 0; x < size; x++ {
+			result.data[y*size+x] = data[x][y]
 			log.Printf("(%d, %d) = %f", x, y, data[x][y])
 		}
 	}
@@ -30,5 +30,20 @@ func NewMatrix(data [][]float64) *Matrix {
 
 // Get gets the value on the specified x,y position
 func (m Matrix) Get(x int, y int) float64 {
-	return m.data[y*m.width+x]
+	return m.data[y*m.size+x]
+}
+
+// Equals checks if the Matrix is equal to another Matrix
+func (m Matrix)Equals(other Matrix) bool {
+	if m.size != other.size {
+		return false
+	}
+	for row := 0; row < m.size; row++ {
+		for col := 0; col < m.size; col++ {
+			if m.data[row*m.size+col] != other.data[row*m.size+col] {
+				return false
+			}
+		}
+	}
+	return true
 }
--- matrix/matrix_test.go
+++ matrix/matrix_test.go
@@ -36,11 +36,125 @@ func Test_Construct_and_Inspect_4x4_Matrix(t *testing.T) {
 		{3, 2, 15.5},
 	}
 	// Then
+	if m.size != 4 {
+		t.Errorf("Size of Matrix = %d, wanted %d", m.size, 4)
+	}
 	for i := 0; i < len(cases); i++ {
 		c := cases[i]
 		v := m.Get(int(c[0]), int(c[1]))
 		if v != c[2] {
 			t.Errorf("Matrix[%d, %d] = %f, wanted %f", int(c[0]), int(c[1]), v, c[2])
 		}
+	}
+}
+
+// Scenario: A 2x2 matrix ought to be representable
+// Given the following 2x2 matrix M:
+// | -3 | 5 |
+// | 1 | -2 |
+// Then the size of M is 2
+func Test_2x2_Matrix(t *testing.T) {
+	// Given
+	m := NewMatrix([][]float64{
+		{-3, 5},
+		{1, -2},
+	})
+	// Expected
+	cases := [][]float64{
+		{0, 0, -3},
+		{0, 1, 5},
+		{1, 0, 1},
+		{1, 1, -2},
+	}
+	// Then
+	if m.size != 2 {
+		t.Errorf("Size of Matrix = %d, wanted %d", m.size, 2)
+	}
+	for i := 0; i < len(cases); i++ {
+		c := cases[i]
+		v := m.Get(int(c[0]), int(c[1]))
+		if v != c[2] {
+			t.Errorf("Matrix[%d, %d] = %f, wanted %f", int(c[0]), int(c[1]), v, c[2])
+		}
+	}
+}
+
+// Scenario: A 3x3 matrix ought to be representable
+// Given the following 3x3 matrix M:
+// | -3 | 5 | 0 |
+// | 1 | -2 | -7 |
+// | 0 | 1 | 1 |
+// Then the size of M is 3
+func Test_3x3_Matrix(t *testing.T) {
+	// Given
+	m := NewMatrix([][]float64{
+		{-3, 5, 0},
+		{1, -2, -7},
+		{0, 1, 1},
+	})
+	// Expected
+	cases := [][]float64{
+		{0, 0, -3},
+		{0, 1, 5},
+		{1, 0, 1},
+		{1, 2, -7},
+		{2, 1, 1},
+		{2, 2, 1},
+	}
+	// Then
+	if m.size != 3 {
+		t.Errorf("Size of Matrix = %d, wanted %d", m.size, 3)
+	}
+	for i := 0; i < len(cases); i++ {
+		c := cases[i]
+		v := m.Get(int(c[0]), int(c[1]))
+		if v != c[2] {
+			t.Errorf("Matrix[%d, %d] = %f, wanted %f", int(c[0]), int(c[1]), v, c[2])
+		}
+	}
+}
+
+// Scenario: Multiplying two matrices
+// Given the following matrix A:
+// | 1 | 2 | 3 | 4 |
+// | 2 | 3 | 4 | 5 |
+// | 3 | 4 | 5 | 6 |
+// | 4 | 5 | 6 | 7 |
+// And the following matrix B:
+// | 0 | 1 | 2 | 4 |
+// | 1 | 2 | 4 | 8 |
+// | 2 | 4 | 8 | 16 |
+// | 4 | 8 | 16 | 32 |
+// Then A * B is the following 4x4 matrix:
+// | 24 | 49 | 98 | 196 |
+// | 31 | 64 | 128 | 256 |
+// | 38 | 79 | 158 | 316 |
+// | 45 | 94 | 188 | 376 |
+func Test_Matrix_Multiplication(t *testing.T) {
+	// Given
+	ma := NewMatrix([][]float64{
+		{ 1, 2, 3, 4 },
+		{ 2, 3, 4, 5 },
+		{ 3, 4, 5, 6 },
+		{ 4, 5, 6, 7 },
+	})
+	mb := NewMatrix([][]float64{
+		{ 0, 1, 2, 4 },
+		{ 1, 2, 4, 8 },
+		{ 2, 4, 8, 16 },
+		{ 4, 8, 16, 32 },
+	})
+	// Expected
+	wanted := NewMatrix([][]float64{
+		{ 24, 49, 98, 196 },
+		{ 31, 64, 128, 256 },
+		{ 38, 79, 158, 316 },
+		{ 45, 94, 188, 376 },
+	})
+	// When
+	r := ma.Multiply(mb)
+	// Then
+	if !wanted.Equals(r) {
+		t.Errorf("%v * %v = %v, wanted %v", ma, mb, r, wanted)
 	}
 }
