b53aa175aac993287a00e537635b918032e1b229
bas <bas@klavertje4.org>
2018-10-05 20:36:51 +0200

    implements Material lighting function

--- materials/materials.go
+++ materials/materials.go
@@ -2,9 +2,12 @@ package materials
 
 import (
 	"fmt"
+	"math"
+
+	"github.com/bas-velthuizen/go-raytracer/lights"
+
 	"github.com/bas-velthuizen/go-raytracer/colors"
 	"github.com/bas-velthuizen/go-raytracer/tuples"
-	"math"
 )
 
 // Materual defines the properties of a material
@@ -29,12 +32,46 @@ func DefaultMaterial() Material {
 
 func (m Material) Equals(other Material) bool {
 	return m.Color.Equals(other.Color) &&
-		math.Abs(m.Ambient - other.Ambient) <= tuples.Epsilon &&
-		math.Abs(m.Diffuse - other.Diffuse) <= tuples.Epsilon &&
-		math.Abs(m.Specular - other.Specular) <= tuples.Epsilon &&
-		math.Abs(m.Shininess - other.Shininess) <= tuples.Epsilon
+		math.Abs(m.Ambient-other.Ambient) <= tuples.Epsilon &&
+		math.Abs(m.Diffuse-other.Diffuse) <= tuples.Epsilon &&
+		math.Abs(m.Specular-other.Specular) <= tuples.Epsilon &&
+		math.Abs(m.Shininess-other.Shininess) <= tuples.Epsilon
 }
 
 func (m Material) String() string {
 	return fmt.Sprintf("Material( %v, %9.6f, %9.6f, %9.6f, %9.6f )", m.Color, m.Ambient, m.Diffuse, m.Specular, m.Shininess)
+}
+
+// Lighting calculates the effective color of a pixel with reflections of light
+func (m Material) Lighting(
+	light lights.PointLight,
+	position tuples.Tuple,
+	eyeV tuples.Tuple,
+	normalV tuples.Tuple,
+) colors.Color {
+	diff := colors.NewColor(0, 0, 0)
+	spec := colors.NewColor(0, 0, 0)
+
+	effectiveColor := m.Color.Blend(light.Intensity)
+	lightV := light.Position.Subtract(position).Normalize()
+
+	ambient := effectiveColor.Multiply(m.Ambient)
+
+	lightDotNormal := lightV.Dot(normalV)
+
+	if lightDotNormal < 0 {
+		diff = colors.NewColor(0, 0, 0)
+		spec = colors.NewColor(0, 0, 0)
+	} else {
+		diff = effectiveColor.Multiply(m.Diffuse * lightDotNormal)
+		reflectV := normalV.Reflect(lightV.Negate())
+		reflectDotEye := math.Pow(reflectV.Dot(eyeV), m.Shininess)
+		if reflectDotEye <= 0 {
+			spec = colors.NewColor(0, 0, 0)
+		} else {
+			spec = light.Intensity.Multiply(m.Specular * reflectDotEye)
+		}
+	}
+	return ambient.Add(diff).Add(spec)
+
 }
