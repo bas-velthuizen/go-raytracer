94d9a8f4aabb68387ca1052397b36c73a0344ca9
bas <bas@klavertje4.org>
2018-10-09 20:28:21 +0200

    started hit precomputation (WIP)

--- rays/intersections_test.go
+++ rays/intersections_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/bas-velthuizen/go-raytracer/spheres"
+	"github.com/bas-velthuizen/go-raytracer/tuples"
 )
 
 // Scenario: An intersection encapsulates `t` and `object`
@@ -53,5 +54,40 @@ func Test_Aggregating_Intersections(t *testing.T) {
 	// And
 	if 2.0 != (*xs)[1].Time {
 		t.Errorf("xs[1] = %9.6f, expected %9.6f", 2.0, (*xs)[1].Time)
+	}
+}
+
+// Scenario: Precomputing the state of an intersection
+// Given ray ← ray(point(0, 0, -5), vector(0, 0, 1))
+// And shape ← sphere()
+// And hit ← intersection(4, shape)
+// When prepare_hit(hit, ray)
+// Then hit.point = point(0, 0, -1)
+// And hit.eyev = vector(0, 0, -1)
+// And hit.normalv = vector(0, 0, -1)
+func Test_Precomputing_the_State_of_an_Intersection(t *testing.T) {
+	// Given
+	ray := NewRay(tuples.Point(0, 0, -5), tuples.Vector(0, 0, 1))
+	// And
+	shape := spheres.NewUnitSphere()
+	// And
+	hit := NewIntersection(4, shape)
+	// When
+	hit.Prepare(ray)
+	// Expected
+	wantedP := tuples.Point(0, 0, -1)
+	wantedE := tuples.Vector(0, 0, -1)
+	wantedN := tuples.Vector(0, 0, -1)
+	// Then
+	if !hit.Point.Equals(wantedP) {
+
+	}
+	// And
+	if !hit.Eyev.Equals(wantedE) {
+
+	}
+	// And
+	if !hit.Normalv.Equals(wantedN) {
+
 	}
 }
